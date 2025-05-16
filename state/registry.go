f7cd45c2966516713953bbe11383026d20762e15
ivanzzeth <ivanzz.eth@gmail.com>
2025-05-16 19:55:39 +0800

    Add default registry and lockerGenerator

--- state/registry.go
+++ state/registry.go
@@ -1,7 +1,19 @@
 package state
 
+var (
+	defaultRegistry Registry = NewSimpleRegistry()
+)
+
 type Registry interface {
 	RegisterState(state State) error
 	GetRegisteredStates() []State
 	NewState(name string) (State, error)
+}
+
+func GetDefaultRegistry() Registry {
+	return defaultRegistry
+}
+
+func SetDefaultRegistry(registry Registry) {
+	defaultRegistry = registry
 }
